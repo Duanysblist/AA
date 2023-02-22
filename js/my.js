87e50489ccae3559b5f22a8758335725788b38d6
Daniel Duany <dduany919@gmail.com>
2023-02-22 18:49:54 -0500

    Started work on the Gallery page to include a carousel

--- js/my.js
+++ js/my.js
@@ -1,18 +1,5 @@
+
 $(document).ready(function () {
-    // $('#AA-title').hide();
-    // $('#navbar-AA').hide();
-    // $('#home-pic').hide();
-    // $('#original-hand-painted-text').hide();
-    // $('#goal').hide();
-    // $('#home-links').hide();
-    // $('footer').hide();
-    // $('#AA-title').fadeIn(3000);
-    // $('#navbar-AA').fadeIn(4000);
-    // $('#home-pic').fadeIn(5000);
-    // $('#original-hand-painted-text').fadeIn(6000);
-    // $('#goal').fadeIn(7000);
-    // $('#home-links').fadeIn(5000);
-    // $('footer').fadeIn(5000);
 
 // Get the modal
     var modal = document.getElementById("myModal");
@@ -35,12 +22,13 @@ $(document).ready(function () {
 
 
 // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
+
 
 
 });
@@ -55,4 +43,8 @@ function openNav() {
 function closeNav() {
     document.getElementById("hamburger-side-nav").style.width = "0";
 }
+
+
+
+
 
